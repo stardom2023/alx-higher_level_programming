5adfbf6085df324436582f6c37fd983e2ebec6de
stardom23 <joy.offor@yahoo.com>
2023-10-09 17:16:32 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
             [1, 2, 3],
             [4, 5, 6],
             [7, 8, 9]
+    ]
 
-            ]
     print_matrix_integer(matrix)
     print("--")
     print_matrix_integer()

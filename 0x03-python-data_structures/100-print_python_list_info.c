e26781c49898f3f7f98d2f004b00750718852064
stardom23 <joy.offor@yahoo.com>
2023-10-09 17:13:03 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - Prints basic information about a Python list
+ * @p: Pointer to the Python list.
+ */
+
+void print_python_list_into(PyObject *p)
+{
+	PyListObject *list;
+	Py_ssize_t size, i;
+	PyObject *item;
+
+	list = (PyListObject *)p;
+	size = Py_SIZE(list);
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}

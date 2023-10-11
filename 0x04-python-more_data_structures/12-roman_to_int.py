f0b88a93dd28a744d9a52a9181268714b97d52ba
stardom23 <joy.offor@yahoo.com>
2023-10-11 16:06:53 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,17 +2,17 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
-    roman_dict = {
-        'I': 1,'V': 5, 'X': 10, 'L': 50,
+
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
 
     result = 0
     prev_value = 0
 
-    for char in roman_string[::-1]:
-        value = roman_dict[char]
+    for char in reversed(roman_string):
+        value = roman_numerals.get(char, 0)
 
         if value < prev_value:
             result -= value
@@ -20,5 +20,22 @@ def roman_to_int(roman_string):
             result += value
 
         prev_value = value
-        
+
     return result
+
+# Test cases
+if __name__ == '__main__':
+    roman_number = "X"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "VII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "IX"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "LXXXVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+    roman_number = "DCCVII"
+    print("{} = {}".format(roman_number, roman_to_int(roman_number)))

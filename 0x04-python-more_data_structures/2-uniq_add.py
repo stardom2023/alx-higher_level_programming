53757f8c7a09fcc912cb1b9a9de122f33c1199c0
stardom23 <joy.offor@yahoo.com>
2023-10-11 07:55:05 -0700

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_set = set()
+    total = 0
+    for num in my_list:
+        if num not in unique_set:
+            unique_set.add(num)
+            total += num
+    return total

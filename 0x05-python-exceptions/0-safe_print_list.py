778433888788dbcdf8057f5a054b37a203278fd9
stardom2023 <joy.ofor@yahoo.com>
2023-10-23 06:03:34 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    real_count = 0
+
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            real_count += 1
+        except IndexError:
+            break
+    print()
+
+    return real_count
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This project tells us more about Python Exceptions. This entails 11 exercises to understand the concept of exceptions in python. 

86f2ef2ec8926743d597a2d16d6e381fff06a47e
stardom2023 <joy.ofor@yahoo.com>
2023-11-06 06:00:02 -0800

    four

--- 0x0A-python-inheritance/4-main.py
+++ 0x0A-python-inheritance/4-main.py
@@ -2,6 +2,7 @@
 inherits_from = __import__('4-inherits_from').inherits_from
 
 a = True
+
 if inherits_from(a, int):
     print("{} inherited from class {}".format(a, int.__name__))
 if inherits_from(a, bool):

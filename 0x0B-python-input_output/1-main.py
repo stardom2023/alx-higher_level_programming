6f67bdeaf083286c0909ea9b2558496c916681fb
stardom2023 <joy.ofor@yahoo.com>
2023-11-07 11:09:48 -0800

    one

--- 0x0B-python-input_output/1-main.py
+++ 0x0B-python-input_output/1-main.py
@@ -1,4 +1,5 @@
-mport__('1-write_file').write_file
+#!/usr/bin/python3
+write_file = __import__('1-write_file').write_file
 
 nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
 print(nb_characters)

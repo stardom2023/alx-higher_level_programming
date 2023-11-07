31397d0736bf3b93a251e63b07369965c054109c
stardom2023 <joy.ofor@yahoo.com>
2023-11-07 12:04:51 -0800

    five

--- 0x0B-python-input_output/5-main.py
+++ 0x0B-python-input_output/5-main.py
@@ -7,20 +7,20 @@ save_to_json_file(my_list, filename)
 
 filename = "my_dict.json"
 my_dict = { 
-            'id': 12,
-                'name': "John",
-                    'places': [ "San Francisco", "Tokyo" ],
-                        'is_active': True,
-                            'info': {
-                                        'age': 36,
-                                                'average': 3.14
-                                                    }
-                            }
+    'id': 12,
+    'name': "John",
+    'places': [ "San Francisco", "Tokyo" ],
+    'is_active': True,
+    'info': {
+        'age': 36,
+        'average': 3.14
+    }
+}
 save_to_json_file(my_dict, filename)
 
 try:
-        filename = "my_set.json"
-            my_set = { 132, 3 }
-                save_to_json_file(my_set, filename)
+    filename = "my_set.json"
+    my_set = { 132, 3 }
+    save_to_json_file(my_set, filename)
 except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    print("[{}] {}".format(e.__class__.__name__, e))

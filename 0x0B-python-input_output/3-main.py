0c83bff0aa721f2f5ec7a015f1ac8f799d578d45
stardom2023 <joy.ofor@yahoo.com>
2023-11-07 11:40:31 -0800

    three

--- 0x0B-python-input_output/3-main.py
+++ 0x0B-python-input_output/3-main.py
@@ -1,4 +1,5 @@
-on_string = __import__('3-to_json_string').to_json_string
+#!/usr/bin/python3
+to_json_string = __import__('3-to_json_string').to_json_string
 
 my_list = [1, 2, 3]
 s_my_list = to_json_string(my_list)
@@ -20,9 +21,9 @@ print(s_my_dict)
 print(type(s_my_dict))
 
 try:
-        my_set = { 132, 3 }
-            s_my_set = to_json_string(my_set)
-                print(s_my_set)
-                    print(type(s_my_set))
+    my_set = { 132, 3 }
+    s_my_set = to_json_string(my_set)
+    print(s_my_set)
+    print(type(s_my_set))
 except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    print("[{}] {}".format(e.__class__.__name__, e))
